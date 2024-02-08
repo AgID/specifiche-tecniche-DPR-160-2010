2dc8b0d6a0ec266ae406d4f200c1fe19b905fc28
vintra73 <vincenzo.travascio@gmail.com>
2024-02-08 17:21:13 +0100

    approved 08/02/2024

--- README.md
+++ README.md
@@ -2,3 +2,44 @@
 ## di cui all’articolo 5 dell’Allegato al Decreto del Presidente della Repubblica 7 settembre 2010, n. 160 e s.m.i.
 
 Il presente repository contiene gli artefatti tecnici (BPMN, immagini, json-schema, openApi e sequence diagram) delle specifiche tecniche di cui all’articolo 5 dell’Allegato al Decreto del Presidente della Repubblica 7 settembre 2010, n. 160 e s.m.i.
+
+
+Il presente repository contiene gli artefatti tecnici (BPMN, immagini, json-schema, openApi e sequence diagram) delle specifiche tecniche di cui all’articolo 5 dell’Allegato al Decreto del Presidente della Repubblica 7 settembre 2010, n. 160 e s.m.i.
+
+
+Il presente branch di lavoro contiene le variazioni alla suddette specifiche tecniche avviate dal Gruppo Tecnico, di cui all'indicato articolo 5, a partire dal 18 gennaio 2024.
+
+
+### Proposte di aggiornamento
+
+I membri del Gruppo Tecnico, o soggetto da loro indicato, POSSONO inserire una issue per formulare una proposta di aggiornamento delle specifiche tecniche.
+
+Al momento dell'inserimento di una issue, alle stesse DEVE essere aggiunta la label *to_be_evaluated* e DOVREBBERO essere classificate utilizzando le label:
+
+- *fixing object schema*: modifica di un object schema quale renaming o aggiunta/rimozione di attributi;
+- *fixing key object*: cambiamento/aggiunta elementi di finding delle entità scambiate tra le componenti, ad esempio metadati gestiti dal Catalogo SSU;
+- *fixing mandatory object*: cambiamento di obbligatorietà di uno o più campi e/o object schema;
+- *add OpenAPI path*: aggiunta di un nuovo path, operazione resa disponibili da un Erogatore ad una categoria di Fruitori, alle OpenAPI;
+- *fixing text writing*: cambiamenti testuali, versione delle specifiche navigabili, di elementi che non appartengono agli artefatti tecnici quali, ad esempio, le OpenAPI).
+
+Il processo di valutazione, analisi e risoluzione utilizzato dal Gruppo Tecnico è riportato nel seguente state diagram.
+
+```mermaid
+  stateDiagram-v2
+    [*] --> to_be_evaluated
+    to_be_evaluated --> cancelled: il GT non ritiene opportuna la proposta
+    cancelled --> [*]
+    to_be_evaluated --> to_analyze: il GT avvia l'analisi della proposta
+    to_analyze --> cancelled: il GT non ritiene opportuna la proposta
+    to_analyze --> propose_solution: il GT avvia la raccolta di soluzioni
+    propose_solution --> approved_solution: il GT approva la soluzione 
+    approved_solution --> [*]
+```
+
+### Presentazione soluzione
+I membri del Gruppo Tecnico, o soggetto da loro indicato, POSSONO presentare una soluzione relativa ad una issue nello stato *propose_solution*.
+
+Per presentare una soluzione utilizzano lo strumento di *pull request*, DEVE:
+
+- utilizzare il *branch* allo stesso assegnato nel rispetto della seguente naming convention [Ente di riferimento]-[Iniziale Nome][Cognome];
+- effettuare una *pull request* dal *branch* al *branch* **mantaince001**.
---
+++ catalogo-ssu/cssu-v1.5.sql
@@ -0,0 +1,342 @@
+CREATE TABLE "administrative_regime" (
+  "k_id" serial4 PRIMARY KEY NOT NULL,
+  "id" varchar(255) NOT NULL,
+  "version" varchar(255) NOT NULL,
+  "description" varchar(255) NOT NULL,
+  "max_gg_proc" int4,
+  "max_gg_admissibility" int4,
+  "max_gg_int_resp" int4,
+  "max_gg_int_req" int4,
+  "max_gg_concl_send" int4,
+  "max_gg_cdss_req" int4,
+  "created_by" int4,
+  "last_modified_by" int4,
+  "dt_last_modified_date" date
+);
+
+CREATE TABLE "bo_system" (
+  "k_id" serial4 PRIMARY KEY NOT NULL,
+  "id" varchar(255) NOT NULL,
+  "version" varchar(255) NOT NULL,
+  "description" varchar(255) NOT NULL,
+  "dt_validity_start_date" date,
+  "dt_validity_end_date" date,
+  "dt_suspended_date" date,
+  "dt_revoke_date" date,
+  "state" varchar,
+  "client_id" varchar,
+  "created_by" int4,
+  "last_modified_by" int4,
+  "dt_last_modified_date" date,
+  "e_service_toet" varchar NOT NULL,
+  "e_service_tofo" varchar NOT NULL,
+  "e_service_tori" varchar NOT NULL
+);
+
+CREATE TABLE "competent_administration_work" (
+  "k_id" serial4 PRIMARY KEY NOT NULL,
+  "fk_competent_administration" int4 NOT NULL,
+  "fk_competent_administration_class" int4 NOT NULL,
+  "dt_start" date,
+  "dt_end" date
+);
+
+CREATE TABLE "competent_administration_territorial_scope" (
+  "k_id" serial4 PRIMARY KEY NOT NULL,
+  "fk_competent_administration" int4 NOT NULL,
+  "fk_territorial_scope" int4 NOT NULL,
+  "dt_start" date,
+  "dt_end" date
+);
+
+CREATE TABLE "competent_administration_class" (
+  "k_id" serial4 PRIMARY KEY NOT NULL,
+  "description" varchar(255) NOT NULL
+);
+
+CREATE TABLE "fo_system" (
+  "k_id" serial4 PRIMARY KEY NOT NULL,
+  "id" varchar(255) NOT NULL,
+  "version" varchar(255) NOT NULL,
+  "description" varchar(255) NOT NULL,
+  "dt_validity_start_date" date,
+  "dt_validity_end_date" date,
+  "dt_suspended_date" date,
+  "dt_revoke_date" date,
+  "state" varchar,
+  "client_id" varchar,
+  "created_by" int4,
+  "last_modified_by" int4,
+  "dt_last_modified_date" date,
+  "e_service_tobo" varchar NOT NULL,
+  "e_service_tocu" varchar NOT NULL
+);
+
+CREATE TABLE "life_event" (
+  "k_id" serial4 PRIMARY KEY NOT NULL,
+  "description" varchar(255) NOT NULL,
+  "id" varchar NOT NULL
+);
+
+CREATE TABLE "schematron" (
+  "k_id" serial4 PRIMARY KEY NOT NULL,
+  "id" varchar(255) NOT NULL,
+  "name" varchar(255) NOT NULL,
+  "version" varchar(255) NOT NULL,
+  "description" varchar(255) NOT NULL,
+  "resource" varchar(255),
+  "status" varchar(255),
+  "fk_territorial_scope" int4,
+  "created_by" int4,
+  "last_modified_by" int4,
+  "dt_last_modified_date" date,
+  "object_type" varchar
+);
+
+CREATE TABLE "territorial_scope" (
+  "k_id" serial4 PRIMARY KEY NOT NULL,
+  "municipality" varchar(255) NOT NULL,
+  "istat" varchar(8),
+  "level" varchar(255),
+  "fk_parent" int4
+);
+
+CREATE TABLE "third_party_system" (
+  "k_id" serial4 PRIMARY KEY NOT NULL,
+  "id" varchar(255) NOT NULL,
+  "version" varchar(255) NOT NULL,
+  "description" varchar(255) NOT NULL,
+  "telematic_ref" varchar(255) NOT NULL,
+  "dt_validity_start_date" date,
+  "dt_validity_end_date" date,
+  "dt_suspended_date" date,
+  "dt_revoke_date" date,
+  "state" varchar,
+  "client_id" varchar,
+  "created_by" int4,
+  "last_modified_by" int4,
+  "dt_last_modified_date" date
+);
+
+CREATE TABLE "typology" (
+  "k_id" serial4 PRIMARY KEY NOT NULL,
+  "description" varchar(255) NOT NULL,
+  "id" varchar NOT NULL
+);
+
+CREATE TABLE "xsd" (
+  "k_id" serial4 PRIMARY KEY NOT NULL,
+  "id" varchar(255) NOT NULL,
+  "name" varchar(255) NOT NULL,
+  "version" varchar(255) NOT NULL,
+  "description" varchar(255) NOT NULL,
+  "resource" varchar(255),
+  "status" varchar(255),
+  "fk_territorial_scope" int4,
+  "created_by" int4,
+  "last_modified_by" int4,
+  "dt_last_modified_date" date,
+  "object_type" varchar
+);
+
+CREATE TABLE "attachment" (
+  "k_id" serial4 PRIMARY KEY NOT NULL,
+  "id" varchar(255) NOT NULL,
+  "fk_schematron" int4,
+  "fk_xsd" int4,
+  "version" varchar(255) NOT NULL,
+  "description" varchar(255) NOT NULL,
+  "status" varchar(255),
+  "created_by" int4,
+  "last_modified_by" int4,
+  "dt_last_modified_date" date,
+  "structured" bool NOT NULL DEFAULT true,
+  "fk_territorial_scope" int4
+);
+
+CREATE TABLE "competent_administration" (
+  "k_id" serial4 PRIMARY KEY NOT NULL,
+  "ipacode" varchar(255) NOT NULL,
+  "office_code" varchar(255),
+  "fk_system_entity" int4 NOT NULL,
+  "version" varchar(255) NOT NULL,
+  "description" varchar(255) NOT NULL,
+  "pec" varchar(255) NOT NULL,
+  "dt_validity_start_date" date,
+  "dt_validity_end_date" date,
+  "created_by" int4,
+  "last_modified_by" int4,
+  "dt_last_modified_date" date
+);
+
+CREATE TABLE "form" (
+  "k_id" serial4 PRIMARY KEY NOT NULL,
+  "id" varchar(255) NOT NULL,
+  "version" varchar(255) NOT NULL,
+  "name" varchar(255) NOT NULL,
+  "description" varchar(255) NOT NULL,
+  "fk_schematron" int4,
+  "fk_xsd" int4,
+  "created_by" int4,
+  "last_modified_by" int4,
+  "dt_last_modified_date" date,
+  "structured" bool NOT NULL DEFAULT true,
+  "state" varchar(255) NOT NULL,
+  "fk_territorial_scope" int4
+);
+
+CREATE TABLE "proceeding" (
+  "k_id" serial4 PRIMARY KEY NOT NULL,
+  "id" varchar(255) NOT NULL,
+  "fk_typology" int4 NOT NULL,
+  "fk_life_event" int4 NOT NULL,
+  "fk_territorial_scope" int4 NOT NULL,
+  "fk_form" int4 NOT NULL,
+  "version" varchar(255) NOT NULL,
+  "description" varchar(255) NOT NULL,
+  "dt_validity_start_date" date,
+  "dt_validity_end_date" date,
+  "created_by" int4,
+  "last_modified_by" int4,
+  "dt_last_modified_date" date,
+  "normative_reference" varchar(255) NOT NULL,
+  "fk_competent_administration_class" int4 NOT NULL
+);
+
+CREATE TABLE "proceeding_attachment" (
+  "k_id" serial4 PRIMARY KEY NOT NULL,
+  "fk_proceeding" int4 NOT NULL,
+  "fk_attachment" int4 NOT NULL,
+  "mandatory" bool NOT NULL
+);
+
+CREATE TABLE "single_desk" (
+  "k_id" serial4 PRIMARY KEY NOT NULL,
+  "id" varchar(255) NOT NULL,
+  "fk_bo_system" int4 NOT NULL,
+  "fk_fo_system" int4 NOT NULL,
+  "version" varchar(255) NOT NULL,
+  "description" varchar(255) NOT NULL,
+  "fk_territorial_scope" int4 NOT NULL,
+  "dt_validity_start_date" date,
+  "dt_validity_end_date" date,
+  "created_by" int4,
+  "last_modified_by" int4,
+  "dt_last_modified_date" date
+);
+
+CREATE TABLE "territorial_scope_user" (
+  "k_id" serial4 PRIMARY KEY NOT NULL,
+  "fk_territorial_scope" int4 NOT NULL,
+  "description" varchar(255) NOT NULL
+);
+
+CREATE TABLE "usecase_proceeding" (
+  "k_id" serial4 PRIMARY KEY NOT NULL,
+  "id" varchar(255) NOT NULL,
+  "fk_proceeding" int4 NOT NULL,
+  "version" varchar(255) NOT NULL,
+  "description" varchar(255) NOT NULL,
+  "schematron_phase" varchar(255),
+  "max_gg_proc" int4,
+  "max_gg_admissibility" int4,
+  "max_gg_int_resp" int4,
+  "max_gg_int_req" int4,
+  "max_gg_concl_send" int4,
+  "max_gg_cdss_req" int4,
+  "fk_administrative_regime" int4 NOT NULL,
+  "created_by" int4,
+  "last_modified_by" int4,
+  "dt_last_modified_date" date,
+  "state" varchar(255) NOT NULL
+);
+
+CREATE TABLE "user" (
+  "k_id" serial4 PRIMARY KEY NOT NULL,
+  "fk_territorial_scope_user" int4 NOT NULL,
+  "name" varchar(255) NOT NULL,
+  "surname" varchar(255) NOT NULL,
+  "fiscal_code" varchar(16) NOT NULL,
+  "role" varchar(255) NOT NULL,
+  "dt_start" date,
+  "dt_end" date,
+  "dt_last_modified_date" date
+);
+
+CREATE TABLE "endoproceeding" (
+  "k_id" serial4 PRIMARY KEY NOT NULL,
+  "fk_primary" int4 NOT NULL,
+  "fk_secondary" int4 NOT NULL,
+  "mandatory" bool NOT NULL,
+  "created_by" int4,
+  "last_modified_by" int4,
+  "dt_last_modified_date" date
+);
+
+CREATE TABLE "belonging_schematron" (
+  "k_id" serial4 PRIMARY KEY NOT NULL,
+  "fk_primary" int4 NOT NULL,
+  "fk_secondary" int4
+);
+
+CREATE TABLE "belonging_xsd" (
+  "k_id" serial4 PRIMARY KEY NOT NULL,
+  "fk_primary" int4 NOT NULL,
+  "fk_secondary" int4
+);
+
+ALTER TABLE "attachment" ADD CONSTRAINT "fk_schematron" FOREIGN KEY ("fk_schematron") REFERENCES "schematron" ("k_id");
+
+ALTER TABLE "attachment" ADD CONSTRAINT "fk_xsd" FOREIGN KEY ("fk_xsd") REFERENCES "xsd" ("k_id");
+
+ALTER TABLE "territorial_scope" ADD CONSTRAINT "fk_parent" FOREIGN KEY ("fk_parent") REFERENCES "territorial_scope" ("k_id");
+
+ALTER TABLE "competent_administration_work" ADD CONSTRAINT "fk_competent_administration_class_adm" FOREIGN KEY ("fk_competent_administration") REFERENCES "competent_administration" ("k_id");
+
+ALTER TABLE "competent_administration_work" ADD CONSTRAINT "fk_competent_administration_class_cls" FOREIGN KEY ("fk_competent_administration_class") REFERENCES "competent_administration_class" ("k_id");
+
+ALTER TABLE "competent_administration_territorial_scope" ADD CONSTRAINT "fk_competent_administration_ref_adm" FOREIGN KEY ("fk_competent_administration") REFERENCES "competent_administration" ("k_id");
+
+ALTER TABLE "competent_administration_territorial_scope" ADD CONSTRAINT "fk_competent_administration_ref" FOREIGN KEY ("fk_territorial_scope") REFERENCES "territorial_scope" ("k_id");
+
+ALTER TABLE "competent_administration" ADD CONSTRAINT "fk_system_entity" FOREIGN KEY ("fk_system_entity") REFERENCES "third_party_system" ("k_id");
+
+ALTER TABLE "form" ADD CONSTRAINT "fk_schematron" FOREIGN KEY ("fk_schematron") REFERENCES "schematron" ("k_id");
+
+ALTER TABLE "form" ADD CONSTRAINT "fk_xsd" FOREIGN KEY ("fk_xsd") REFERENCES "xsd" ("k_id");
+
+ALTER TABLE "proceeding" ADD CONSTRAINT "fk_tipology" FOREIGN KEY ("fk_typology") REFERENCES "typology" ("k_id");
+
+ALTER TABLE "proceeding" ADD CONSTRAINT "fk_life_event" FOREIGN KEY ("fk_life_event") REFERENCES "life_event" ("k_id");
+
+ALTER TABLE "proceeding" ADD CONSTRAINT "fk_territorial_scope" FOREIGN KEY ("fk_territorial_scope") REFERENCES "territorial_scope" ("k_id");
+
+ALTER TABLE "proceeding" ADD CONSTRAINT "fk_form" FOREIGN KEY ("fk_form") REFERENCES "form" ("k_id");
+
+ALTER TABLE "proceeding_attachment" ADD CONSTRAINT "fk_proceeding" FOREIGN KEY ("fk_proceeding") REFERENCES "proceeding" ("k_id");
+
+ALTER TABLE "proceeding_attachment" ADD CONSTRAINT "fk_attachment" FOREIGN KEY ("fk_attachment") REFERENCES "attachment" ("k_id");
+
+ALTER TABLE "single_desk" ADD CONSTRAINT "fk_bo_system" FOREIGN KEY ("fk_bo_system") REFERENCES "bo_system" ("k_id");
+
+ALTER TABLE "single_desk" ADD CONSTRAINT "fk_fo_system" FOREIGN KEY ("fk_fo_system") REFERENCES "fo_system" ("k_id");
+
+ALTER TABLE "territorial_scope_user" ADD CONSTRAINT "fk_territorial_scope" FOREIGN KEY ("fk_territorial_scope") REFERENCES "territorial_scope" ("k_id");
+
+ALTER TABLE "usecase_proceeding" ADD CONSTRAINT "fk_proceeding" FOREIGN KEY ("fk_proceeding") REFERENCES "proceeding" ("k_id");
+
+ALTER TABLE "usecase_proceeding" ADD CONSTRAINT "fk_administrative_regime" FOREIGN KEY ("fk_administrative_regime") REFERENCES "administrative_regime" ("k_id");
+
+ALTER TABLE "user" ADD CONSTRAINT "fk_territorial_scope_user" FOREIGN KEY ("fk_territorial_scope_user") REFERENCES "territorial_scope_user" ("k_id");
+
+ALTER TABLE "endoproceeding" ADD CONSTRAINT "fk_primary" FOREIGN KEY ("fk_primary") REFERENCES "usecase_proceeding" ("k_id");
+
+ALTER TABLE "endoproceeding" ADD CONSTRAINT "fk_secondary" FOREIGN KEY ("fk_secondary") REFERENCES "usecase_proceeding" ("k_id");
+
+ALTER TABLE "belonging_schematron" ADD CONSTRAINT "belonging_schematron_fk" FOREIGN KEY ("fk_primary") REFERENCES "schematron" ("k_id");
+
+ALTER TABLE "belonging_schematron" ADD CONSTRAINT "belonging_schematron_fk_1" FOREIGN KEY ("fk_secondary") REFERENCES "schematron" ("k_id");
+
+ALTER TABLE "single_desk" ADD CONSTRAINT "fk_territorial_scope" FOREIGN KEY ("fk_territorial_scope") REFERENCES "territorial_scope" ("k_id");
+
+ALTER TABLE "proceeding" ADD CONSTRAINT "fk_proceeding_class" FOREIGN KEY ("fk_competent_administration_class") REFERENCES "competent_administration_class" ("k_id");
